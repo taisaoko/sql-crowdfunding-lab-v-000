fcd0d391ff5203bff4d194e6b02ab30a2f68468e
taisaoko <ttd3calvin@gmail.com>
2018-11-20 23:25:44 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal REAL,
+start_date TEXT,
+end_date TEXT
+);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledge (id INTEGER PRIMARY KEY, amount REAL, user_id INTEGER, project_id INTEGER);
